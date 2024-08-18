cb8581e3fde845527eda3e4a79ffe4eb31f58c1f
Raphael Fleury <raphael.fleury9@gmail.com>
2024-08-18 17:52:18 -0300

    feat: add address and veterinarian models

---
+++ src/app/models/address.model.ts
@@ -0,0 +1,8 @@
+export type Address = {
+  country: string,
+  postalCode: string,
+  street: string,
+  number?: number | null,
+  city: string,
+  state: string
+}
---
+++ src/app/models/veterinarian.model.ts
@@ -0,0 +1,8 @@
+import { Address } from "./address.model";
+
+export type Veterinarian = {
+  name: string,
+  email: string,
+  phone: string,
+  address: Address,
+}
