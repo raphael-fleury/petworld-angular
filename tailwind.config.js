d2879631edcd056742dcf840ddf16b247e467fe9
Raphael Fleury <raphael.fleury9@gmail.com>
2024-08-18 15:31:00 -0300

    feat: add global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pw-1': '#FFFFFF',
+        'pw-2': '#FFEACD',
+        'pw-3': '#FFC14D',
+        'pw-4': '#FFA400',
+        'pw-5': '#EB751E',
+        'pw-6': '#C55C1B',
+        'pw-7': '#6B2206',
+        'pw-8': '#000000',
+      },
+    }
   },
   plugins: [],
 }
